9385c5a6d3358abbf57738c8cca758783ca75b80
Bouquet <julien.bouquet@epsi.fr>
2018-02-14 21:04:08 +0100

    feat(header): add header and nav bar

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../models/User';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  private user: User = {
+    firstName: 'Julien',
+    lastName: 'Bouquet',
+    email: 'julien.bouquet@live.fr',
+    job: 'Developpeur Full-Stack',
+    about: 'Je suis étudiant en ingénerie informatique en 5ème année à l\'EPSI Lille. Je suis en alternance en tant que développeur full-stack pour la société ECONOCOM.',
+    idGitHub: 'julien-bouquet',
+    idTwitter: 'julien_bouquet'
+  };
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/models/User.ts
@@ -0,0 +1,15 @@
+export class User {
+
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  job?: string;
+  about?: string;
+  idGitHub?: string;
+  idTwitter?: string;
+  idStackOverFlow?: string;
+
+  constructor() {
+
+  }
+}
